d33d59bfc0c74e65247b74b5d2cb74ddcfd0913b
smitkapadiya07 <smitkapadiya07@gmail.com>
2025-07-28 14:45:05 +0530

    DashboardHeader

--- src/components/dashboard/DashboardHeader.tsx
+++ src/components/dashboard/DashboardHeader.tsx
@@ -110,13 +110,13 @@ export const DashboardHeader = ({toggleSidebar}: DashboardHeaderProps) => {
                 {/* Right section */}
                 <div className="flex items-center space-x-4">
                     {/* Notification bell */}
-                    <Button variant="ghost" size="sm" className="relative">
-                        <Bell className="h-5 w-5"/>
-                        <span
-                            className="absolute -top-1 -right-1 h-4 w-4 bg-red-500 text-white text-xs rounded-full flex items-center justify-center">
-              3
-            </span>
-                    </Button>
+            {/*        <Button variant="ghost" size="sm" className="relative">*/}
+            {/*            <Bell className="h-5 w-5"/>*/}
+            {/*            <span*/}
+            {/*                className="absolute -top-1 -right-1 h-4 w-4 bg-red-500 text-white text-xs rounded-full flex items-center justify-center">*/}
+            {/*  3*/}
+            {/*</span>*/}
+            {/*        </Button>*/}
 
                     {/* Avatar dropdown */}
                     <DropdownMenu>
